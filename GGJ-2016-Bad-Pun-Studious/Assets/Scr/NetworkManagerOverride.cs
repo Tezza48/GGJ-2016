46af6f3f3eac2f01b67c047a9c42cf4a358c606a
William Terry <Tezza48is@aol.com>
2016-01-30 11:40:43 +0000

    now spawning an object to pick what is the right player prefab

---
+++ GGJ-2016-Bad-Pun-Studious/Assets/Scr/NetworkManagerOverride.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class NetworkManagerOverride : NetworkManager {
+    public GameObject ghostPrefab;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        GameObject newPlayerPrefab;
+
+        var tempPlayer = GameObject.FindGameObjectWithTag("Player");
+
+        if (tempPlayer == null)
+        {
+            newPlayerPrefab = playerPrefab;
+        }
+        else
+        {
+            newPlayerPrefab = ghostPrefab;
+        }
+
+        var player = (GameObject)GameObject.Instantiate(newPlayerPrefab, Vector3.zero, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
---
+++ GGJ-2016-Bad-Pun-Studious/Assets/Scr/PlayerPicker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class PlayerPicker : NetworkBehaviour {
+
+    public GameObject playerPrefab;
+    public GameObject ghostPrefab;
+
+	// Use this for initialization
+	void Start () {
+        GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
+        if (currentPlayer == null)
+        {
+            Spawn(playerPrefab);
+        }
+        else
+        {
+            Spawn(ghostPrefab);
+        }
+    }
+
+    private void Spawn(GameObject prefab)
+    {
+        GameObject playerSpawn = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
+        NetworkServer.Spawn(playerSpawn);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
