fac490668aefc38ed9c7960c45737035495462ba
William Terry <Tezza48is@aol.com>
2016-01-31 02:49:28 +0000

    alpha of terrain with trigger on it goes down when covering player

---
+++ GGJ-2016-Bad-Pun-Studious/Assets/Scr/Rendering/TerrainTriggerOpacity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainTriggerOpacity : MonoBehaviour {
+
+    private float minimumAlpha = 0.2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Terrain"))
+        {
+            SpriteRenderer sprite = other.GetComponent<SpriteRenderer>();
+            Color spriteColor = sprite.color;
+            spriteColor.a = minimumAlpha;
+            sprite.color = spriteColor;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Terrain"))
+        {
+            SpriteRenderer sprite = other.GetComponent<SpriteRenderer>();
+            Color spriteColor = sprite.color;
+            spriteColor.a = 1;
+            sprite.color = spriteColor;
+        }
+    }
+}
