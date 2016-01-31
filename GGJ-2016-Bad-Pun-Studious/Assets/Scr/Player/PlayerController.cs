c88a0253d749808746a10140e217024db54327f4
William Terry <Tezza48is@aol.com>
2016-01-31 15:56:03 +0000

    colls

--- GGJ-2016-Bad-Pun-Studious/Assets/Scr/Player/PlayerController.cs
+++ GGJ-2016-Bad-Pun-Studious/Assets/Scr/Player/PlayerController.cs
@@ -12,14 +12,4 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 	
 	}
-
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-
-        if (Input.GetButtonDown("Use") && other.tag == "Portal")
-        {
-            transform.position = other.GetComponent<PortalEntrance>().exit.position;
-        }
-    }
 }
